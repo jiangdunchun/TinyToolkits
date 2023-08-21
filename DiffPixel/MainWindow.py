d0edb86e8a2dad85ebc6f26e454d037b69bed7a0
JiangDunchun <jiangdunchun@outlook.com>
2023-08-21 21:14:24 +0800

    remove some files

--- DiffPixel/MainWindow.py
+++ DiffPixel/MainWindow.py
@@ -95,9 +95,11 @@ class MainWindow(QWidget):
 
 
 import sys
+from qt_material import apply_stylesheet
 
 if __name__ == "__main__":  
     app = QApplication(sys.argv)  
+    apply_stylesheet(app, theme='dark_teal.xml')
     win = MainWindow()  
     win.show()  
     sys.exit(app.exec_())
